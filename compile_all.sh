7f03ba206eb90737e60a974f19dd7248d34f60d6
Riccardo Petraglia <riccardo.petraglia@epfl.ch>
2015-11-20 07:39:56 +0100

    compile working better now

--- compile_all.sh
+++ compile_all.sh
@@ -12,6 +12,17 @@ echo "MAKE CLEAN"
 make distclean > log_clean
 make fullclean >> log_clean
 
+echo ''
+echo 'All non-tracked file will be removed by git clean'
+echo 'Do you want to run git clean? [y/N]'
+read answer
+if [[ "$answer" == 'y' ]]; then
+  git clean -fd
+  git clean -fX
+  git clean -f
+  git clean -fdX
+fi
+
 echo "Coping everything on the cluster"
 rsync -uha $PRJT_RT/ $CLUSTER_COMPILE_DIR
 
@@ -23,6 +34,8 @@ set -o nounset
 set -e pipefail
 
 echo "COMPILING ON LCMDLC2"
+
+module load intelmpi/5.1.1
 
 cd ${CLUSTER_COMPILE_DIR/md2:/}/
 ./configure --prefix=\$PWD/lcmdlc2 &> log_lcmdlc2
@@ -36,14 +49,19 @@ set -e pipefile
 echo "Coping back everything from the cluster"
 rsync -uha $CLUSTER_COMPILE_DIR/ $PRJT_RT
 
-
 echo "COMPILING HERE"
 cd $PRJT_RT
-#make distclean
-#make fullclean
+mv lcmdlc2 ../tmp-$RND
+if [[ "$answer" == 'y' ]]; then
+  git clean -fd
+  git clean -fX
+  git clean -f
+  git clean -fdX
+fi
 ./configure --prefix=$PWD/local &> log_local
 make -j 16 &>> log_local
 make install &>> log_local
+mv ../tmp-$RND lcmdlc2
 
 echo "SYNCRONIZE"
 clusters_sync
